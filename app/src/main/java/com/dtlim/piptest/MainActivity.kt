a5dab21bdfb94f41682fb2b1ae4f5adc99cd3d36
Dale Lim <dale.lim@foxtel.com.au>
2021-08-30 02:23:41 +1000

    Initial commit

---
+++ app/src/main/java/com/dtlim/piptest/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dtlim.piptest
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dtlim.piptest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonVideo.setOnClickListener {
+            startActivity(Intent(this, VideoActivity::class.java))
+        }
+    }
+}
